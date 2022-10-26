f8a08f24ecfb876f7a7534488371499716abf1ee
Amangggganteng <103506892+Amangggganteng@users.noreply.github.com>
2022-10-27 00:33:39 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -14,11 +14,11 @@ PHOTO = "https://telegra.ph/file/08fdbf96f37505efd36f2.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hai [{event.sender.first_name}](tg://user?id={event.sender.id}), Saya AMG-Robot.** \n\n"
-  TEXT += "⚪ **Saya Bekerja Dengan Benar** \n\n"
-  TEXT += f"⚪ **Juragan Ku : [ᴀᴍᴀɴɢ](https://t.me/@mwahnq)** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += " **Saya Bekerja Dengan Benar** \n\n"
+  TEXT += f" **Juragan Ku : [ᴀᴍᴀɴɢ](https://t.me/mwahnq)** \n\n"
+  TEXT += f" **Library Version :** `{telever}` \n\n"
+  TEXT += f" **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f" **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Terima Kasih Telah Menambahkan Saya Di Sini ❤️**"
   BUTTON = [[Button.url("Help", "https://t.me/amanqss?start=help"), Button.url("Donasi", "https://t.me/mwahnq")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
